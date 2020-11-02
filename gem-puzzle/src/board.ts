4ba6dbac0be2e02ba43cccd2e6708b1af998355b
Gregory Moskalev <onberfrest@gmail.com>
2020-11-02 15:08:00 +0300

    feat: render board

--- gem-puzzle/src/board.ts
+++ gem-puzzle/src/board.ts
@@ -1,5 +1,17 @@
-const createBoard = () => {
-  return 'hello!'
+function createBoard(size: number = 4):any {
+  const board = [];
+  for(let i= 0; i< size; ++i){
+    board[i]=[];
+    for(let j = 0;j < size; ++j){
+      if(i+j !== (size -1) * 2){
+        board[i][j] = i*size + j + 1;
+      } else {
+        board[i][j]= 0;
+      }
+    }
+  }
+
+  return board;
 };
 
 export default createBoard;
--- gem-puzzle/src/index.ts
+++ gem-puzzle/src/index.ts
@@ -2,8 +2,17 @@ import board from './board';
 
 function component() {
   const element = document.createElement('div');
+  element.classList.add('board');
+  const arr = board()
+  let size = 4;
+  console.log(arr);
+  
+  for(let i = 0; i< size; i++){
+    for(let j = 0; j < size; j++){
+      element.innerHTML += `<div class='cell'>${arr[i][j]}</div>`;
+    }
+  }
 
-  element.innerHTML = board();
 
   return element;
 }
